e9e0780b88f38d61d6c863a42500fa01eb1645ec
David Hyman <david.hyman@arm.com>
2017-12-27 17:21:22 +0000

    Potential uses state instead of flags

--- src/resource_locker/core/potential.py
+++ src/resource_locker/core/potential.py
@@ -2,8 +2,7 @@ class Potential:
     def __init__(self, item, key_gen=None, **params):
         self._key = item if key_gen is None else key_gen(item)
         self.item = item
-        self._is_fulfilled = False
-        self._is_rejected = False
+        self._state = None
 
     @property
     def key(self):
@@ -11,21 +10,20 @@ class Potential:
 
     @property
     def is_fulfilled(self):
-        return self._is_fulfilled
+        return self._state is True
 
     @property
     def is_rejected(self):
-        return self._is_rejected
+        return self._state is False
 
     def fulfill(self):
-        self._is_fulfilled = True
+        self._state = True
         return self
 
     def reject(self):
-        self._is_rejected = True
+        self._state = False
         return self
 
     def reset(self):
-        self._is_fulfilled = False
-        self._is_rejected = False
+        self._state = None
         return self
